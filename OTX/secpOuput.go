8af48b098e5cb0ebb5b17d0575614a4059fa3afa
Zaki Manian <zaki@manian.org>
2016-12-09 09:29:07 -0800

    Output serializationx

--- OTX/secpOuput.go
+++ OTX/secpOuput.go
@@ -11,20 +11,13 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 package OTX
 
 import (
-	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"errors"
-	"fmt"
 
 	"github.com/btcsuite/btcd/btcec"
-	"github.com/golang/protobuf/proto"
-	"github.com/skuchain/popcodes_utxo/Popcodes"
-	"github.com/skuchain/popcodes_utxo/ProofElementStore"
 )
 
 type SecP256k1Output struct {
-	Popcode   *Popcodes.Popcode
 	Owners    []btcec.PublicKey
 	OutputIdx int
 	Threshold int
@@ -33,10 +26,9 @@ type SecP256k1Output struct {
 	Creator   btcec.PublicKey
 }
 
-func New(parent *Popcodes.Popcode, creator btcec.PublicKey, idx int, amount int, data string) SecP256k1Output {
+func New(creator btcec.PublicKey, idx int, amount int, data string) SecP256k1Output {
 	code := SecP256k1Output{}
-	code.Popcode = parent
-	code.OutputIdx = id
+	code.OutputIdx = idx
 	code.Data = data
 	code.Amount = amount
 	code.Creator = creator
@@ -53,226 +45,133 @@ func (b *SecP256k1Output) PubKeys() []string {
 	return output
 }
 
-func (b *SecP256k1Output) verifySigs(message string, signatures *[][]byte) (bool, []btcec.Signature) {
-	validSig := false
-	validatedSigs := *new([]btcec.Signature)
-	usedKeys := make([]bool, len(b.PublicKeys))
-	messageBytes := sha256.Sum256([]byte(message))
-	for _, sigbytes := range *signatures {
+// func (b *SecP256k1Output) verifySigs(message string, signatures *[][]byte) (bool, []btcec.Signature) {
+// 	validSig := false
+// 	validatedSigs := *new([]btcec.Signature)
+// 	usedKeys := make([]bool, len(b.PublicKeys))
+// 	messageBytes := sha256.Sum256([]byte(message))
+// 	for _, sigbytes := range *signatures {
 
-		signature, err := btcec.ParseDERSignature(sigbytes, btcec.S256())
-		if err != nil {
-			fmt.Println("Bad signature encoding")
-			return false, nil
-		}
+// 		signature, err := btcec.ParseDERSignature(sigbytes, btcec.S256())
+// 		if err != nil {
+// 			fmt.Println("Bad signature encoding")
+// 			return false, nil
+// 		}
 
-		for i, pubKey := range b.PublicKeys {
-			success := signature.Verify(messageBytes[:], &pubKey)
-			if success && (usedKeys[i] == false) {
-				validSig = true
-				validatedSigs = append(validatedSigs, *signature)
-				usedKeys[i] = true
-			}
-		}
-	}
+// 		for i, pubKey := range b.PublicKeys {
+// 			success := signature.Verify(messageBytes[:], &pubKey)
+// 			if success && (usedKeys[i] == false) {
+// 				validSig = true
+// 				validatedSigs = append(validatedSigs, *signature)
+// 				usedKeys[i] = true
+// 			}
+// 		}
+// 	}
 
-	if validSig == false {
-		return false, nil
-	}
-	if len(validatedSigs) < b.Threshold {
-		return false, nil
-	}
-	return validSig, validatedSigs
-}
-func (b *SecP256k1Output) Supercede(signatures *[][]byte, supercededBy string, supercedingName string) bool {
+// 	if validSig == false {
+// 		return false, nil
+// 	}
+// 	if len(validatedSigs) < b.Threshold {
+// 		return false, nil
+// 	}
+// 	return validSig, validatedSigs
+// }
 
-	success, sigs := b.verifySigs(b.ProofName+":superceded:"+supercededBy, signatures)
-	if !success {
-		return false
-	}
+// func (b *SecP256k1Output) ToBytes() []byte {
+// 	store := ProofElementStore.SECPProofElementStore{}
+// 	store.Name = b.ProofName
+// 	store.Data = b.Data
+// 	store.SupercededBy = b.SupercededBy
+// 	store.Threshold = int32(b.Threshold)
+// 	switch b.State {
+// 	case Initialized:
+// 		store.State = ProofElementStore.SECPProofElementStore_Initialized
+// 	case Signed:
+// 		store.State = ProofElementStore.SECPProofElementStore_Signed
+// 	case Revoked:
+// 		store.State = ProofElementStore.SECPProofElementStore_Revoked
+// 	case Superceded:
+// 		store.State = ProofElementStore.SECPProofElementStore_Superceded
+// 	}
 
-	if b.State == Initialized || b.State == Signed || b.State == Revoked {
-		b.State = Superceded
-		b.SupercededBy = supercedingName
-		b.Signatures = sigs
-		return true
-	}
-	return false
-}
+// 	for _, key := range b.PublicKeys {
+// 		store.PublicKeys = append(store.PublicKeys, key.SerializeCompressed())
+// 	}
+// 	for _, sigs := range b.Signatures {
+// 		store.Signatures = append(store.Signatures, sigs.Serialize())
+// 	}
+// 	metastore := ProofElementStore.ProofElementStore{}
+// 	metastore.Type = ProofElementStore.ProofElementStore_SECP
+// 	metastore.Secp = &store
 
-func (b *SecP256k1Output) Revoked(signatures *[][]byte) bool {
+// 	bufferBytes, err := proto.Marshal(&metastore)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	return bufferBytes
+// }
 
-	success, sigs := b.verifySigs(b.ProofName+":revoked", signatures)
-	if !success {
-		return false
-	}
-
-	if b.State == Initialized || b.State == Signed {
-		b.State = Revoked
-		b.Signatures = sigs
-		return true
-	}
-	return false
-}
-
-func (b *SecP256k1Output) Signed(signatures *[][]byte, data string) bool {
-
-	success, sigs := b.verifySigs(b.ProofName+":"+data, signatures)
-	if !success {
-		return false
-	}
-	if b.State == Initialized {
-		b.State = Signed
-		b.Signatures = sigs
-		b.Data = data
-		return true
-	}
-	return false
-}
-
-func (b *SecP256k1Output) Fulfillment() string {
-
-	return "Not Implemented Yet"
-}
-
-func (b *SecP256k1Output) ToBytes() []byte {
-	store := ProofElementStore.SECPProofElementStore{}
-	store.Name = b.ProofName
-	store.Data = b.Data
-	store.SupercededBy = b.SupercededBy
-	store.Threshold = int32(b.Threshold)
-	switch b.State {
-	case Initialized:
-		store.State = ProofElementStore.SECPProofElementStore_Initialized
-	case Signed:
-		store.State = ProofElementStore.SECPProofElementStore_Signed
-	case Revoked:
-		store.State = ProofElementStore.SECPProofElementStore_Revoked
-	case Superceded:
-		store.State = ProofElementStore.SECPProofElementStore_Superceded
-	}
-
-	for _, key := range b.PublicKeys {
-		store.PublicKeys = append(store.PublicKeys, key.SerializeCompressed())
-	}
-	for _, sigs := range b.Signatures {
-		store.Signatures = append(store.Signatures, sigs.Serialize())
-	}
-	metastore := ProofElementStore.ProofElementStore{}
-	metastore.Type = ProofElementStore.ProofElementStore_SECP
-	metastore.Secp = &store
-
-	bufferBytes, err := proto.Marshal(&metastore)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return bufferBytes
-}
-
-func (b *SecP256k1Output) FromBytes(bits []byte) error {
-	metastore := ProofElementStore.ProofElementStore{}
-	err := proto.Unmarshal(bits, &metastore)
-	if err != nil {
-		return err
-	}
-	if metastore.Type != ProofElementStore.ProofElementStore_SECP {
-		return errors.New("Expected SECP proof")
-	}
-	store := metastore.Secp
-	b.ProofName = store.Name
-	b.Data = store.Data
-	b.SupercededBy = store.SupercededBy
-	b.Threshold = int(store.Threshold)
-	switch store.State {
-	case ProofElementStore.SECPProofElementStore_Initialized:
-		b.State = Initialized
-	case ProofElementStore.SECPProofElementStore_Signed:
-		b.State = Signed
-	case ProofElementStore.SECPProofElementStore_Revoked:
-		b.State = Revoked
-	case ProofElementStore.SECPProofElementStore_Superceded:
-		b.State = Superceded
-	}
-	for _, key := range store.PublicKeys {
-		publicKey, err := btcec.ParsePubKey(key, btcec.S256())
-		if err != nil {
-			return err
-		}
-		b.PublicKeys = append(b.PublicKeys, *publicKey)
-	}
-	for _, sig := range store.Signatures {
-		signature, err := btcec.ParseSignature(sig, btcec.S256())
-		if err != nil {
-			return err
-		}
-		b.Signatures = append(b.Signatures, *signature)
-	}
-	return nil
-}
+// func (b *SecP256k1Output) FromBytes(bits []byte) error {
+// 	metastore := ProofElementStore.ProofElementStore{}
+// 	err := proto.Unmarshal(bits, &metastore)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if metastore.Type != ProofElementStore.ProofElementStore_SECP {
+// 		return errors.New("Expected SECP proof")
+// 	}
+// 	store := metastore.Secp
+// 	b.ProofName = store.Name
+// 	b.Data = store.Data
+// 	b.SupercededBy = store.SupercededBy
+// 	b.Threshold = int(store.Threshold)
+// 	switch store.State {
+// 	case ProofElementStore.SECPProofElementStore_Initialized:
+// 		b.State = Initialized
+// 	case ProofElementStore.SECPProofElementStore_Signed:
+// 		b.State = Signed
+// 	case ProofElementStore.SECPProofElementStore_Revoked:
+// 		b.State = Revoked
+// 	case ProofElementStore.SECPProofElementStore_Superceded:
+// 		b.State = Superceded
+// 	}
+// 	for _, key := range store.PublicKeys {
+// 		publicKey, err := btcec.ParsePubKey(key, btcec.S256())
+// 		if err != nil {
+// 			return err
+// 		}
+// 		b.PublicKeys = append(b.PublicKeys, *publicKey)
+// 	}
+// 	for _, sig := range store.Signatures {
+// 		signature, err := btcec.ParseSignature(sig, btcec.S256())
+// 		if err != nil {
+// 			return err
+// 		}
+// 		b.Signatures = append(b.Signatures, *signature)
+// 	}
+// 	return nil
+// }
 
 func (b *SecP256k1Output) ToJSON() []byte {
 	type JSONBracket struct {
-		ProofName    string
-		State        string
-		Signatures   []string
-		PublicKeys   []string
-		SupercededBy string
-		Threshold    int
-		Data         string
+		Owners    []string
+		Threshold int
+		Data      string
+		Creator   string
+		Amount    int
 	}
 	jsonBracket := JSONBracket{}
 
-	jsonBracket.ProofName = b.ProofName
-	switch b.State {
-	case Initialized:
-		jsonBracket.State = "Initialized"
-	case Signed:
-		jsonBracket.State = "Signed"
-	case Revoked:
-		jsonBracket.State = "Revoked"
-	case Superceded:
-		jsonBracket.State = "Superceded"
+	for _, pubKey := range b.Owners {
+		jsonBracket.Owners = append(jsonBracket.Owners, hex.EncodeToString(pubKey.SerializeCompressed()))
 	}
-	for _, sig := range b.Signatures {
-		jsonBracket.Signatures = append(jsonBracket.Signatures, hex.EncodeToString(sig.Serialize()))
-	}
-	for _, pubKey := range b.PublicKeys {
-		jsonBracket.PublicKeys = append(jsonBracket.PublicKeys, hex.EncodeToString(pubKey.SerializeCompressed()))
-	}
-	jsonBracket.SupercededBy = b.SupercededBy
 	jsonBracket.Threshold = b.Threshold
 	jsonBracket.Data = b.Data
+	jsonBracket.Amount = b.Amount
 
 	jsonstring, err := json.Marshal(jsonBracket)
 	if err != nil {
 		return nil
 	}
 	return jsonstring
-}
-
-func (b *SecP256k1Output) VerifyIdentities(idKeys []btcec.PublicKey, uuid string) bool {
-	hashedUUID := sha256.Sum256([]byte(uuid))
-	doubleHashedUUID := sha256.Sum256(hashedUUID[:])
-	encodedHash := hex.EncodeToString(doubleHashedUUID[:])
-	if encodedHash != b.ProofName {
-		return false
-	}
-	curve := btcec.S256()
-	offsetX, offsetY := curve.ScalarBaseMult(hashedUUID[:])
-	usedKeys := make([]bool, len(b.PublicKeys))
-	countUsed := 0
-
-	for _, idkey := range idKeys {
-		offsettedX, offsettedY := curve.Add(idkey.X, idkey.Y, offsetX, offsetY)
-		for i, key := range b.PublicKeys {
-			if key.X == offsettedX && key.Y == offsettedY {
-				usedKeys[i] = true
-				countUsed++
-			}
-		}
-	}
-	if countUsed == len(b.PublicKeys) {
-		return true
-	}
-	return false
 }
