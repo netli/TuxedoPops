9c456fe087a729189cda1a966b98708f6ade5757
Zaki Manian <zaki@manian.org>
2017-02-09 17:51:57 -0800

    Begining of a test file

---
+++ popcodesChaincode_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+func checkInit(t *testing.T, stub *shim.MockStub, args []string) {
+	_, err := stub.MockInit("1", "", args)
+	if err != nil {
+		fmt.Println("INIT", args, "failed", err)
+		t.FailNow()
+	}
+}
+
+func checkInvoke(t *testing.T, stub *shim.MockStub, args []string) {
+	_, err := stub.MockInvoke("1", "UPSERT", args)
+	if err != nil {
+		fmt.Println("UPSERT", args, "failed", err)
+		t.FailNow()
+	}
+}
+
+func checkQuery(t *testing.T, stub *shim.MockStub, name string, value string) {
+	bytes, err := stub.MockQuery("QUERY", []string{name})
+	if err != nil {
+		fmt.Println("Query", name, "failed", err)
+		t.FailNow()
+	}
+	if bytes == nil {
+		fmt.Println("Query", name, "failed to get value")
+		t.FailNow()
+	}
+	if string(bytes) != value {
+		fmt.Println("Query value", name, "was not", value, "as expected")
+		t.FailNow()
+	}
+}
+
+func TestPopcodeChaincode(t *testing.T) {
+	bst := new(popcodesChaincode)
+	stub := shim.NewMockStub("bst", bst)
+	checkInvoke(t, stub, []string{`{"uuid":"1234","title":"test"}`})
+	checkQuery(t, stub, "1234", `{"uuid":"1234","title":"test"}`)
+}
